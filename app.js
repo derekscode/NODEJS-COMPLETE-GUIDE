ad9073a8f5dd04123a5a0764eab32e701b371abd
derekscode <17056283+derekscode@users.noreply.github.com>
2020-09-19 00:02:12 -0700

    finished 35

--- app.js
+++ app.js
@@ -24,11 +24,10 @@ const server = http.createServer((req, res) => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
 
   res.setHeader("Content-Type", "text/html");
